8df3297e1f4e66dc500a67affa1926a7b0ba3cf8
Alejandro Gallo <aamsgallo@gmail.com>
2022-03-19 19:51:44 +0100

    Update Mafiel

--- 2022-PRACE-paris/config.mk
+++ 2022-PRACE-paris/config.mk
@@ -1,1 +1,19 @@
-all: tex/vasp-ctf.pdf
+SINGLE_AMPLS = \
+	lisp/t-1.dot \
+	lisp/t-1.svg \
+	lisp/t-2.dot \
+	lisp/t-2.svg \
+	lisp/t-3.dot \
+	lisp/t-3.svg \
+	lisp/t-4.dot \
+	lisp/t-4.svg \
+	lisp/t-5.dot \
+	lisp/t-5.svg \
+
+$(SINGLE_AMPLS): lisp/tn.lisp
+	cd $(@D) && sbcl --script $(notdir $<)
+
+all: \
+	tex/vasp-ctf.pdf \
+	$(SINGLE_AMPLS) \
+
