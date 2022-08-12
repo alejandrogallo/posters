2bf36ad61773be33fa9bb5384655425f3bf95e35
Alejandro Gallo <aamsgallo@gmail.com>
2022-08-12 03:48:46 +0200

    Update Menton

--- 2018-menton/config.mk
+++ 2018-menton/config.mk
@@ -1,4 +1,4 @@
 #DEPENDENCIES = baposter.cls
 BUILD_DIR = .build
 
-main.pdf: figures/boron_orbitals/orbitals.pdf
+main.pdf: figures/boron_orbitals/orbitals.pdf ./figures/boron_energies/plot.pdf figures/background.pdf figures/boron_orbitals/orbitals.pdf figures/boron_energies/plot.pdf figures/dmrg_results.pdf figures/dmrg_results.pdf figures/POSCAR_16_view.png figures/simple_orbital_pictures/basic_level_definition_with_spin_3.pdf
